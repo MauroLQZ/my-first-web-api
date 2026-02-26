cf02e8fe2bf4f4850f64c99f41665c1e515e91d1
maurolqz@gmail.com <maurolqz@gmail.com>
2026-02-26 09:50:17 -0300

    feat:update classes repositorio web-api.26/02/26-09:50.
    Merge remote-tracking branch 'origin/main' into main

---
+++ my-first-web-api/src/main/java/dio/web/api/repository/UsuarioRepository.java
@@ -0,0 +1,53 @@
+package dio.web.api.repository;
+
+import org.springframework.stereotype.Repository;
+
+import dio.web.api.model.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UsuarioRepository {
+	public void save(Usuario usuario){
+		if(usuario.getId() == null)
+			System.out.println("SAVE - Recebendo o usuário na camada de repositório");
+		else
+			System.out.println("UPDATE - Recebendo o usuário na camada de repositório");
+		
+		System.out.println(usuario);
+    }
+	public void deleteById(Integer id){
+        System.out.println(String.format("DELETE/id - Recebendo o id: %d para Excluir um usuário", id));
+        System.out.println(id);
+    }
+	public List<Usuario> findAll(){
+		System.out.println("LIST - Listando os usuarios do sistema!");
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("gleyson","password"));
+        usuarios.add(new Usuario("frank","masterpass"));
+        return usuarios;
+    }
+	public Usuario finById(Integer id){
+        System.out.println(String.format("FIND/id - Recebendo o id: %d para localizar um usuário", id));
+        return new Usuario("gleyson","password");
+    }
+	public Usuario findByUsername(String username){
+        System.out.println(String.format("FIND/Username - Recebendo o username: %s para localizar um usuário", username));
+        return new Usuario("gleyson","password");
+    }
+	/*
+    public List<Usuario> listAll(){
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("gleyson","password"));
+        usuarios.add(new Usuario("frank","masterpass"));
+        return usuarios;
+    }*/
+    /*
+    public void remove(Integer id){
+        System.out.println(String.format("DELETE/id - Recebendo o id: %d para excluir um usuário", id));
+        System.out.println(id);
+    }
+    
+    */
+}
